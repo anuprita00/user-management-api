8e7ed00d7884f7f8fdbb527bb5ff0e980378d527
Anuprita <anupande84@gmail.com>
2025-02-02 18:47:00 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req,res) => {
+app.use(express.json()); // Middleware to parse JSON
+
+app.get("/",(req, res) => {
     res.send("Hello");
 })
 
+let user = [
+    {id: 1, name:"Alice"},
+    {id:2, name:"Bob"}
+]
+
+//Get all users
+app.get("/users", (req, res) => {
+    res.json(user);
+})
+
+//get user by id
+app.get("/users/:id", (req, res) => {
+    
+})
 
 app.listen(3000, () => {
     console.log("running");
